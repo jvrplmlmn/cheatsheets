fda096169ad6a5be6fc4999d3b39a0831982d11e
jvrplmlmn <jvrplmlmn@gmail.com>
2013-05-28 15:29:56 +0300

    Create monitorQueues.py

---
+++ weblogic/monitorQueues.py
@@ -0,0 +1,5 @@
+# WLST / Python script I created to retrieve statistics from some JMS Queues
+# The statistics are dumped to a csv file, and then processed to add a header
+# 
+# Usage:
+# ${WL_HOME}/common/bin/wlst.sh foo.py
